1beb663d1da65c02058b71268d6a605c0b07f81e
jianghutan <jianghutan@aliyun.com>
2019-11-05 15:46:35 +0800

    练习

---
+++ src/main/java/com/jianghutan/jianfa/JianfaApplication.java
@@ -0,0 +1,13 @@
+package com.jianghutan.jianfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JianfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jianghutan/jianfa/JianfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianghutan.jianfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JianfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
